82f3cb08fba567a75ca6957a12e0bf249614f807
fahmi32i <fahmimagdi45@gmail.com>
2025-01-14 18:37:26 +0200

    testReady

---
+++ src/app/app.routes.ts
@@ -0,0 +1,18 @@
+import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
+import { ContactComponent } from './contact/contact.component';
+import { FooterComponent } from './footer/footer.component';
+import { NotfoundpageComponent } from './notfoundpage/notfoundpage.component';
+
+
+export const routes: Routes = [
+    {path:'',redirectTo:'home',pathMatch:'full'},
+    {path:'home',component:HomeComponent},
+    {path:'about',component:AboutComponent},
+    {path:'portfolio',component:PortfolioComponent},
+    {path:'contact',component:ContactComponent},
+    {path:'footer',component:FooterComponent},
+    {path:'**',component:NotfoundpageComponent},
+];
---
+++ src/app/notfoundpage/notfoundpage.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-notfoundpage',
+  imports: [RouterLink],
+  templateUrl: './notfoundpage.component.html',
+  styleUrl: './notfoundpage.component.css'
+})
+export class NotfoundpageComponent {
+
+}
